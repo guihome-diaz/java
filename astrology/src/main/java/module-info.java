2706c4f51426f2073a849b357088113a5dd69f6b
Guillaume <guillaume@qin-diaz.com>
2019-10-25 22:40:27 +0200

    Completion of Java11 migration

--- astrology/src/main/java/module-info.java
+++ astrology/src/main/java/module-info.java
@@ -2,10 +2,13 @@ module astrology {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.web;
-    requires slf4j.api;
 
-    opens eu.daxiongmao.prv.astrology to slf4j.api, javafx.fxml, javafx.base, javafx.controls, javafx.web, javafx.graphics;
-    opens eu.daxiongmao.prv.astrology.controller to slf4j.api, javafx.fxml, javafx.base, javafx.controls, javafx.web, javafx.graphics;
+    // Because Logback 1.3.x is already Java9 ready and has the latest SLF4J embedded,
+    // you can rely on that single line
+    requires org.slf4j;
+
+    opens eu.daxiongmao.prv.astrology to org.slf4j, javafx.fxml, javafx.base, javafx.controls, javafx.web, javafx.graphics;
+    opens eu.daxiongmao.prv.astrology.controller to org.slf4j, javafx.fxml, javafx.base, javafx.controls, javafx.web, javafx.graphics;
 
     exports eu.daxiongmao.prv.astrology;
 }
